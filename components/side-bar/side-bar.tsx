9181f9d2d3487ecb0aa124bdc4595e2864b5517d
Jack Cascarini <11981133+cascabeanie@users.noreply.github.com>
2024-12-18 17:57:23 +0000

    style: add border to tab button for visibility

--- components/side-bar/side-bar.tsx
+++ components/side-bar/side-bar.tsx
@@ -22,19 +22,17 @@ export default function SideBar() {
         } transition-all bg-white border min-h-svh w-full sm:w-3/4 xl:w-1/2 p-4 sm:p-8`}
       >
         <div className="flex justify-center mt-5}">
-          {isSideOpen === false ? (
-            <PanelRightOpen
-              className="fixed z-50 top-3 right-5"
-              size={30}
-              onClick={sideBarHandler}
-            />
-          ) : (
-            <PanelRightClose
-              className="fixed z-50 top-3 right-5 animate-pulse"
-              size={30}
-              onClick={sideBarHandler}
-            />
-          )}
+          <span className="fixed flex items-center justify-center h-10 w-10 rounded-md z-10 top-2 right-5 bg-white border">
+            {isSideOpen === false ? (
+              <PanelRightOpen size={30} onClick={sideBarHandler} />
+            ) : (
+              <PanelRightClose
+                className="animate-pulse"
+                size={30}
+                onClick={sideBarHandler}
+              />
+            )}
+          </span>
 
           <ResultsSearch />
         </div>
